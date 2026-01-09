65cb599c01f41d6474b68c63c15bcb4d974c8105
Ovilli <mzlatin4@gmail.com>
2026-01-09 15:50:28 +0100

    yay

---
+++ tactix/scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+
+func _on_options_pressed() -> void:
+	print("Not Ready Yet")
+
+
+func _on_play_pressed() -> void:
+	print("Start Game")
+	get_tree().change_scene_to_file("res://scenes/main_game.tscn")
