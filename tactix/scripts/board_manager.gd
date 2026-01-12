4190c23a6047ca9e4254d9addf2a821365f8514b
Ovilli <mzlatin4@gmail.com>
2026-01-12 21:14:42 +0100

    alpha 0.2 tactix
    . added cursor
    . added camera ( ai ai ai ai ) for testing prupuses
    . imporved board
    . added descriptions
    . improved visuals
    . fixed bugs
    . created bugs

--- tactix/scripts/board_manager.gd
+++ tactix/scripts/board_manager.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
 
-const BOARD_SIZE =  9
-const CELL_WIDTH = 1.10000002384186
+const BOARD_SIZE =  9 # 9 x 9 Board 
+const CELL_WIDTH = 1.10000002384186 # mesh size damit all genau auf ihr feld passen
 
 const BOX = preload("uid://kin1civoox8r")
 const DICE = preload("uid://cejfohddoqyog")
@@ -10,11 +10,8 @@ const DICE_BLACK = preload("uid://qybkw75qflsr")
 const KING = preload("uid://dyg82xwpwinl3")
 const KING_BLACK = preload("uid://43g6o0d0v0yb")
 
-@onready var _0_0: Marker3D = $"0|0"
+@onready var _0_0: Marker3D = $"0|0" # Marker der 0 0 markiert und woran sich alle figuren orientieren
 
-
-
-# Need some type of State ( Players Turn , Player Selecting Pice , Players_turn) 
 
 var players_turn := true  # if true it is  whites trun , if false it is  blacks turn 
 var board : Array = [] # an list of all positions of the board aswell as what pice is on it 
@@ -48,35 +45,60 @@ func display_board():
 			var piece_id = board[x][y]
 			if piece_id == 0:  # 0 = nichts 
 				continue
-
+				
 			var mesh : Mesh
 			if piece_id > 0 and piece_id < 7: # gucken ob es weiß ist 
-				mesh = DICE
+				mesh = DICE # setzen variable mesh auf DICE => eine weiße figur  wird gepsawnt
 			else:
 				if piece_id == 10: # gucken ob es ein könig ist 
-					mesh = KING
+					mesh = KING # setzen variable mesh auf KING => ein könig wird gepsawnt
 				else:
 					if piece_id < 0 and piece_id > -7: # gucken ob es schwarz ist
-						mesh = DICE_BLACK
-					else: # gucken ob es ein schwarzer könig ist 
-						mesh = KING_BLACK 
+						mesh = DICE_BLACK # setzen variable mesh auf DICE_BLACK => eine schwarze figur wird gepsawnt
+					else:
+						# gucken ob es ein schwarzer könig ist 
+						if piece_id == -10:
+							mesh = KING_BLACK # setzen variable mesh auf KING_BLACK => ein schwarzer  könig wird gepsawnt
 						
-				
-
-			spawn_piece(mesh, x, y)
+			spawn_piece(mesh, x, y, piece_id)
 			print("Spawning piece at:", x, y, mesh)
+			
+func find_rotation_of_piece(piece_id):
+				# https://docs.godotengine.org/en/stable/tutorials/3d/using_transforms.html
+				if piece_id == 1 or piece_id == -1: # ist das pice eine 1 ?
+					# -90 0 0
+					return Vector3(deg_to_rad(-90), 0, 0) # ist das pice eine 2 ?
+				elif piece_id == 2 or piece_id == -2:
+					# 0 0 0
+					return Vector3(0, 0, 0)
+				elif piece_id == 3 or piece_id == -3:# ist das pice eine 3 ?
+					# 0 0 90
+					return Vector3(0, 0, deg_to_rad(90)) # ist das pice eine 4 ?
+				elif piece_id == 4 or piece_id == -4:
+					# 0 0 -90
+					return Vector3(0, 0, deg_to_rad(-90)) # ist das pice eine 5 ?
+				elif piece_id == 5 or piece_id == -5:
+					# 0 0 180
+					return Vector3(0, 0, deg_to_rad(180)) # ist das pice eine 6 ?
+				elif piece_id == 6 or piece_id == -6:
+					# 90 0 0
+					return Vector3(deg_to_rad(90), 0, 0) # ist das pice eine 6 ?
+				return Vector3.ZERO
+								
+func spawn_piece(mesh, x, y, piece_id): # function um ein mesh zu spawnen 
+	# wir mussen nun das 3d model spawnen was dem mesh was wir oben nennen entschpricht 
+	var dice_mesh := MeshInstance3D.new() # sagen variable dice_mesh = ein 3d Mesh Objekt 
+	dice_mesh.mesh = mesh # sagen das mesh = unser vairable (KING; WEIß , SChWARZ )
+	add_child(dice_mesh) # fügen es als "kind" zur unsere main scene hinzu 
 
+	dice_mesh.global_position = _0_0.global_position + Vector3( # wir starten bei 0 0 
+		x * CELL_WIDTH + CELL_WIDTH * 0.5, # wir gehen zur x postion des würfels + die breite des würfels
+		0.0,# wir gehen nicht nach oben
+		y * CELL_WIDTH + CELL_WIDTH * 0.5 # wir gehen zur y postion des würfels + die breite des würfels
+	)
 
-func spawn_piece(mesh:Mesh, x:int, y:int):
-	var instance := MeshInstance3D.new()
-	instance.mesh = mesh
-	add_child(instance)
-
-	instance.global_position = _0_0.global_position + Vector3(
-	x * CELL_WIDTH + CELL_WIDTH * 0.5,
-	0.0,
-	y * CELL_WIDTH + CELL_WIDTH * 0.5
-)
+	dice_mesh.rotation = find_rotation_of_piece(piece_id) # wir rotiren den w+rfel das er immer die richtige seite zeigt
 	
 	
-	
+	#TOD0
+	# Fixen das die Würfel sich nicht beim rotieren bewegen : ( 
