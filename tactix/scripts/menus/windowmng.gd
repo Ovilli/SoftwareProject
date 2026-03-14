d16602e2ff4f656cc7437904663c210985789135
Ovilli <mzlatin4@gmail.com>
2026-03-14 12:21:20 +0100

    Added Sound Effects

--- tactix/scripts/menus/windowmng.gd
+++ tactix/scripts/menus/windowmng.gd
@@ -13,6 +13,8 @@ enum Screen {
 var current: Screen = Screen.NONE
 var previous: Screen = Screen.NONE
 
+const EFFECT = preload("uid://dontb43nxxubn")
+
 signal window_changed(from: Screen, to: Screen)
 
 func open(w: Screen) -> void:
@@ -22,12 +24,23 @@ func open(w: Screen) -> void:
 	previous = current
 	current = w
 	window_changed.emit(previous, current)
-
+	play_effect()
 func close() -> void:
 	open(previous)
+	play_effect()
+	
 
 func is_open(w: Screen) -> bool:
 	return current == w
 
 func is_not_open(w: Screen) -> bool:
 	return current != w
+
+
+func play_effect() -> void:
+	var player = AudioStreamPlayer.new()
+	add_child(player)
+	player.stream = EFFECT
+	player.pitch_scale = randf_range(0.9, 1.1)
+	player.play()
+	player.finished.connect(player.queue_free)
