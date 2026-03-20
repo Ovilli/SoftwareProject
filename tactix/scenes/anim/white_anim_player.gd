45efd20976642727db55c85580c50caa7ed94426
Hell0Tears <minko.gohl@mail.gymnasiumsteglitz.de>
2026-03-20 23:55:17 +0100

    finished the stupid animation
    yayayayayayayayayayayay
    finallyyyyyyy!!!!!!!!!!!!!!!!!!!!!!

--- tactix/scenes/anim/white_anim_player.gd
+++ tactix/scenes/anim/white_anim_player.gd
@@ -44,18 +44,20 @@ func _on_piece_moved(from_x, from_y, delta_x, delta_y):
 	var anim_player = anim_instance.get_node("AnimationPlayer")
 	var rot_mesh = anim_instance.get_node("Pivot/Rot")
 	var face_rot: Vector3
-	
+	var _temp_faces
 	
 	var key = str(from_x) + "|" + str(from_y)
 	if Globals.dice_states.has(key):
 		print(Globals.dice_states[key])
 		face_rot = Globals.find_rotation_of_piece(key)
 		rot_mesh.rotation = face_rot
+		_temp_faces = Globals.dice_states[key].duplicate()
+		
 	if Globals.dice_states[key] == { "top": 10, "bottom": 10, "north": 10, "south": 10, "east": 10, "west": 10 }:
 		piece_mesh = piece.get_node("Pivot/MeshInstance3D")
 	
-	
 	while times_moved != times:
+		
 		if piece and anim_mesh:
 			if piece_mesh:
 				anim_mesh.mesh = piece_mesh.mesh
@@ -64,18 +66,22 @@ func _on_piece_moved(from_x, from_y, delta_x, delta_y):
 		if delta_x > 0:
 			anim_instance.rotation_degrees.y = 0
 			rot_mesh.rotation_degrees.y += 0
+			_temp_faces = _temp_faces.roll_right(_temp_faces)
 			x+=times_moved
 		elif delta_x < 0:
 			anim_instance.rotation_degrees.y = 180
 			rot_mesh.rotation_degrees.y -= 180
+			_temp_faces = TurnMng.roll_left(_temp_faces)
 			x-=times_moved
 		elif delta_y > 0:
 			anim_instance.rotation_degrees.y = -90 
 			rot_mesh.rotation_degrees.y += 90
+			_temp_faces = TurnMng.roll_forward(_temp_faces)
 			y += times_moved
 		elif delta_y < 0:
 			anim_instance.rotation_degrees.y = 90
 			rot_mesh.rotation_degrees.y -= 90
+			_temp_faces = TurnMng.roll_backward(_temp_faces)
 			y-=times_moved
 		
 # TODO: adding the faces &&renewing the pivot of animation
@@ -87,8 +93,9 @@ func _on_piece_moved(from_x, from_y, delta_x, delta_y):
 		
 		anim_player.play("white_rotation")
 		await anim_player.animation_finished
+		rot_mesh.rotation = find_anim_rotation(_temp_faces)
 		times_moved += 1
-	
+
 	anim_instance.queue_free()
 	TurnMng.is_animating = false
 	Globals.display_board()
@@ -100,3 +107,9 @@ func get_piece_at(x, y) -> Node3D:
 		if piece.get_meta("board_x") == x and piece.get_meta("board_y") == y:
 			return piece
 	return null
+	
+func find_anim_rotation(faces):
+	var top = faces.top
+	var north = faces.north
+	var rot_values = Globals.find_top_of_piece(top, north)
+	return Vector3(deg_to_rad(rot_values[0]), deg_to_rad(rot_values[1]), deg_to_rad(rot_values[2]))
